83da747132377823295966557aee8c2ba53d0dfc
saputroandi <57069797+saputroandi@users.noreply.github.com>
2025-04-27 04:08:10 +0700

    first commit

---
+++ src/main/java/com/test/nti/NtiApplication.java
@@ -0,0 +1,14 @@
+package com.test.nti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class NtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/nti/NtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.nti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
